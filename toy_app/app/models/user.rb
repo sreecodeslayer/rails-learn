ca3755af88e584f08745541b2bf54d9fabf7ca61
Sreenadh TC <kesav.tc8@gmail.com>
2020-02-12 11:30:17 +0530

    Add user authentication using bcrypt password hashing, update forms

--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save {self.email = self.email.downcase}
+
   has_many :microposts
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: {maximum: 50}
+  validates :password, presence: true, length: {minimum: 6}
+  validates :email, presence: true, length: {maximum: 250},
+  format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+
+  has_secure_password
 end
